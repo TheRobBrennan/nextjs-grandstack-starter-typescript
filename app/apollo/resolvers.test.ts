ccc162b4b83e8d930d5c08ec1f437554265ec5c8
Rob Brennan <rob@therobbrennan.com>
2020-08-25 18:23:54 -0700

    Added tests for GraphQL Query resolvers

---
+++ app/apollo/resolvers.test.ts
@@ -0,0 +1,22 @@
+import { resolvers } from "./resolvers"
+
+describe("Our GraphQL resolvers should contain", () => {
+  describe("A Query object", () => {
+    const { Query } = resolvers
+
+    it("that has been defined", () => {
+      expect(Query).toBeDefined()
+    })
+
+    describe("with a custom 'hello' function", () => {
+      const { hello } = Query
+      it("that has been defined", () => {
+        expect(hello).toBeDefined()
+      })
+      it("that returns a string when executed", () => {
+        const result = hello(null, null, null)
+        expect(typeof result).toEqual("string")
+      })
+    })
+  })
+})
