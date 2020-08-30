78a2b5364683d758661043b11d4531dbf377f647
Rob Brennan <rob@therobbrennan.com>
2020-08-29 22:31:30 -0700

    Added initial styling and AppBar

--- app/grandstack-demo/App.tsx
+++ app/grandstack-demo/App.tsx
@@ -1,13 +1,66 @@
-import { FC } from "react"
+import { FC, useState } from "react"
+import clsx from "clsx"
 
 // Material UI
-import { Typography } from "@material-ui/core"
+import { useStyles } from "./App.utilities"
+import {
+  AppBar,
+  CssBaseline,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@material-ui/core"
+import {
+  ChevronLeft as ChevronLeftIcon,
+  Menu as MenuIcon,
+  Dashboard as DashboardIcon,
+  People as PeopleIcon,
+} from "@material-ui/icons"
 
 const App: FC = () => {
+  const classes = useStyles()
+
+  // Drawer
+  const [open, setOpen] = useState(true)
+  const handleDrawerOpen = () => {
+    setOpen(true)
+  }
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
+
   return (
-    <Typography variant="body1" color="textSecondary" align="center">
-      [DEMO] GRANDstack Starter
-    </Typography>
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="absolute"
+        className={clsx(classes.appBar, open && classes.appBarShift)}
+      >
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            className={clsx(
+              classes.menuButton,
+              open && classes.menuButtonHidden
+            )}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.title}
+          >
+            GRANDstack Starter
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
   )
 }
 export default App
