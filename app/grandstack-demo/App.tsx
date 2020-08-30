6f7c87a011260f78af55dd26ea272c1f96507616
Rob Brennan <rob@therobbrennan.com>
2020-08-30 00:08:14 -0700

    Removed drawer styling and functionality

--- app/grandstack-demo/App.tsx
+++ app/grandstack-demo/App.tsx
@@ -1,5 +1,5 @@
-import { FC, useState } from "react"
-import Link from "next/link"
+import { FC } from "react"
+import Head from "next/head"
 import clsx from "clsx"
 
 // Material UI
@@ -9,57 +9,23 @@ import {
   Box,
   CssBaseline,
   Container,
-  Divider,
-  Drawer,
-  IconButton,
   Link as MUILink,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
   Toolbar,
-  Tooltip,
   Typography,
 } from "@material-ui/core"
-import {
-  ChevronLeft as ChevronLeftIcon,
-  Menu as MenuIcon,
-  Dashboard as DashboardIcon,
-  People as PeopleIcon,
-} from "@material-ui/icons"
 
 const App: FC = () => {
   const classes = useStyles()
-
-  // Drawer
-  const [open, setOpen] = useState(true)
-  const handleDrawerOpen = () => {
-    setOpen(true)
-  }
-  const handleDrawerClose = () => {
-    setOpen(false)
-  }
+  const APP_TITLE = "[DEMO] GRANDstack Starter"
 
   return (
     <div className={classes.root}>
+      <Head>
+        <title>{APP_TITLE}</title>
+      </Head>
       <CssBaseline />
-      <AppBar
-        position="absolute"
-        className={clsx(classes.appBar, open && classes.appBarShift)}
-      >
+      <AppBar position="absolute" className={clsx(classes.appBar)}>
         <Toolbar className={classes.toolbar}>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            className={clsx(
-              classes.menuButton,
-              open && classes.menuButtonHidden
-            )}
-          >
-            <MenuIcon />
-          </IconButton>
           <img
             className={classes.appBarImage}
             src="img/grandstack.png"
@@ -72,58 +38,20 @@ const App: FC = () => {
             noWrap
             className={classes.title}
           >
-            GRANDstack Starter
+            {APP_TITLE}
           </Typography>
         </Toolbar>
       </AppBar>
-      <Drawer
-        variant="permanent"
-        classes={{
-          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-        }}
-        open={open}
-      >
-        <div className={classes.toolbarIcon}>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          <Link href="/" passHref>
-            <ListItem button>
-              <ListItemIcon>
-                <Tooltip title="Dashboard" aria-label="Dashboard">
-                  <DashboardIcon />
-                </Tooltip>
-              </ListItemIcon>
-              <ListItemText primary="Dashboard" />
-            </ListItem>
-          </Link>
-
-          <Link href="/" passHref>
-            <ListItem button>
-              <ListItemIcon>
-                <Tooltip title="Users" aria-label="Users">
-                  <PeopleIcon />
-                </Tooltip>
-              </ListItemIcon>
-              <ListItemText primary="Users" />
-            </ListItem>
-          </Link>
-        </List>
-        <Divider />
-      </Drawer>
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
           <Box pt={4}>
             <Typography variant="body2" color="textSecondary" align="center">
               {"Copyright © "}
-              <MUILink color="inherit" href="">
-                GRANDstack Starter
+              {new Date().getFullYear()}{" "}
+              <MUILink color="inherit" href="/">
+                {APP_TITLE}
               </MUILink>{" "}
-              {new Date().getFullYear()}
               {"."}
             </Typography>
           </Box>
--- app/grandstack-demo/App.utilities.tsx
+++ app/grandstack-demo/App.utilities.tsx
@@ -1,67 +1,22 @@
 // Material UI
 import { makeStyles } from "@material-ui/core/styles"
 
-// Constants
-export const drawerWidth = 240
-
 // Styling
 export const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
   },
   toolbar: {
-    paddingRight: 24, // keep right padding when drawer closed
-  },
-  toolbarIcon: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    padding: "0 8px",
-    ...theme.mixins.toolbar,
+    paddingRight: 24,
   },
   appBar: {
-    zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: "none",
   },
   title: {
     flexGrow: 1,
-  },
-  drawerPaper: {
-    position: "relative",
-    whiteSpace: "nowrap",
-    width: drawerWidth,
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerPaperClose: {
-    overflowX: "hidden",
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up("sm")]: {
-      width: theme.spacing(9),
-    },
   },
   appBarSpacer: theme.mixins.toolbar,
   content: {
@@ -72,19 +27,6 @@ export const useStyles = makeStyles((theme) => ({
   container: {
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    display: "flex",
-    overflow: "auto",
-    flexDirection: "column",
-  },
-  fixedHeight: {
-    height: 240,
-  },
-  navLink: {
-    textDecoration: "none",
-    color: "inherit",
   },
   appBarImage: {
     maxHeight: "75px",
