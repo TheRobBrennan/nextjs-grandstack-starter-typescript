01fa60ae9f043e3927073dddb5eb1b349e3eb7be
Rob Brennan <rob@therobbrennan.com>
2020-08-29 21:18:12 -0700

    Added server side rendering for Material UI

--- app/pages/_document.js
+++ app/pages/_document.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Document, { Head, Main, NextScript } from "next/document"
+import { ServerStyleSheets } from "@material-ui/styles"
 
 import { GOOGLE_ANALYTICS_TRACKING_ID } from "../analytics/google/googleAnalytics"
 
@@ -8,6 +9,8 @@ class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <meta charSet="utf-8" />
+          <meta name="theme-color" content="#000000" />
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
@@ -32,6 +35,53 @@ class MyDocument extends Document {
         </body>
       </html>
     )
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  // Resolution order
+  // Source https://itnext.io/next-js-with-material-ui-7a7f6485f671
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets()
+  const originalRenderPage = ctx.renderPage
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    })
+
+  const initialProps = await Document.getInitialProps(ctx)
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      <React.Fragment key="styles">
+        {initialProps.styles}
+        {sheets.getStyleElement()}
+      </React.Fragment>,
+    ],
   }
 }
 
