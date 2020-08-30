6792799ce61b3519bbf9f9d4c298884878f88892
Rob Brennan <rob@therobbrennan.com>
2020-08-30 00:59:17 -0700

    Added open graph and meta information

--- app/grandstack-demo/layout/Header/Header.tsx
+++ app/grandstack-demo/layout/Header/Header.tsx
@@ -8,19 +8,66 @@ import { AppBar, CssBaseline, Toolbar, Typography } from "@material-ui/core"
 
 const Header: FC = () => {
   const classes = useStyles()
+
   const APP_TITLE = "Next.js GRANDstack Starter with TypeScript"
+  const APP_DESCRIPTION = "A sample Next.js GRANDstack Starter with TypeScript"
+  const APP_URL = "https://nextjs-grandstack-starter-typescript.vercel.app"
+  const APP_TYPE = "website"
+  const APP_LOGO = "img/grandstack.png"
+  const APP_LOGO_URL = `${APP_URL}/${APP_LOGO}`
+  const APP_TWITTER_ACCOUNT = "therobbrennan"
+  const KEYWORDS = "nextjs, react, grandstack, neo4j, typescript, apollo"
 
   return (
     <>
       <Head>
         <title>{APP_TITLE}</title>
+        <meta
+          property="description"
+          content={APP_DESCRIPTION}
+          key="description"
+        />
+        <meta property="keywords" content={KEYWORDS} key="keywords" />
+        <meta property="og:title" content={APP_TITLE} key="og:title" />
+
+        {/* Open Graph */}
+        <meta
+          property="og:description"
+          content={APP_DESCRIPTION}
+          key="og:description"
+        />
+        <meta property="og:url" content={APP_URL} key="og:url" />
+        <meta property="og:type" content={APP_TYPE} key="og:type" />
+        <meta property="og:image" content={APP_LOGO_URL} key="og:image" />
+
+        {/* Twitter */}
+        <meta
+          property="twitter:creator"
+          content={APP_TWITTER_ACCOUNT}
+          key="twitter:creator"
+        />
+        <meta
+          property="twitter:title"
+          content={APP_TITLE}
+          key="twitter:title"
+        />
+        <meta
+          property="twitter:description"
+          content={APP_DESCRIPTION}
+          key="twitter:description"
+        />
+        <meta
+          property="twitter:image"
+          content={APP_LOGO_URL}
+          key="twitter:image"
+        />
       </Head>
       <CssBaseline />
       <AppBar position="absolute" className={clsx(classes.appBar)}>
         <Toolbar className={classes.toolbar}>
           <img
             className={classes.appBarImage}
-            src="img/grandstack.png"
+            src={APP_LOGO}
             alt="GRANDstack logo"
           />
           <Typography
