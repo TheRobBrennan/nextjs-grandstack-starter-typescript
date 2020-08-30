0cc194e7d7e2fa018ec42a5143b3a8b67d7902c3
Rob Brennan <rob@therobbrennan.com>
2020-08-29 21:05:39 -0700

    Update Next.js config with our plug-ins

--- app/next.config.js
+++ app/next.config.js
@@ -1,8 +1,15 @@
+const path = require("path")
+
+// Plug-ins
+const withPlugins = require("next-compose-plugins")
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 })
+const withImages = require("next-images")
 
-const config = {
+// Combine an array of plug-ins with a Next.js configuration object
+module.exports = withPlugins([[withImages], [withBundleAnalyzer]], {
+  // Custom webpack configuration goes here 🤓
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
@@ -11,8 +18,10 @@ const config = {
       }
     }
 
+    // Necessary for resolving our styles, etc
+    config.resolve.modules.push(path.resolve("./"))
+
+    // Return our custom webpack configuration
     return config
   },
-}
-
-module.exports = withBundleAnalyzer(config)
+})
