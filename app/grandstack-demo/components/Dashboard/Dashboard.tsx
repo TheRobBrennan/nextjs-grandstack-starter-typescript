fb82a606a293e3b66b1bf53bf671e17ab83fb448
Rob Brennan <rob@therobbrennan.com>
2020-08-30 12:35:23 -0700

    Refactored styles into a separate file

--- app/grandstack-demo/components/Dashboard/Dashboard.tsx
+++ app/grandstack-demo/components/Dashboard/Dashboard.tsx
@@ -7,32 +7,17 @@ import UserCount from "../UserCount/UserCount"
 import RecentReviews from "../RecentReviews/RecentReviews"
 
 // Material UI
+import { useStyles } from "./Dashboard.styles"
 import { useTheme } from "@material-ui/core/styles"
 import { Grid, Paper } from "@material-ui/core"
-import { makeStyles } from "@material-ui/core/styles"
 
 const Dashboard = () => {
   const theme = useTheme()
-
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      display: "flex",
-    },
-    paper: {
-      padding: theme.spacing(2),
-      display: "flex",
-      overflow: "auto",
-      flexDirection: "column",
-    },
-    fixedHeight: {
-      height: 240,
-    },
-  }))
   const classes = useStyles(theme)
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight)
 
   return (
-    <React.Fragment>
+    <>
       <Grid container spacing={4}>
         {/* Ratings Chart */}
         <Grid item xs={12} md={8} lg={7}>
@@ -53,7 +38,7 @@ const Dashboard = () => {
           </Paper>
         </Grid>
       </Grid>
-    </React.Fragment>
+    </>
   )
 }
 export default Dashboard
