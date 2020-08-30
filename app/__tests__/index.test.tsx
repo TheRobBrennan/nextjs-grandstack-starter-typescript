84615fd18c9ca653922e3e949c9eb251e6f9974d
Rob Brennan <rob@therobbrennan.com>
2020-08-30 13:24:34 -0700

    Wrap index page in an Apollo client MockedProvider

--- app/__tests__/index.test.tsx
+++ app/__tests__/index.test.tsx
@@ -1,10 +1,16 @@
 import React from "react"
 import { mount } from "enzyme"
+
 import DefaultPage from "../pages/index"
+import { MockedProvider } from "@apollo/client/testing"
 
 describe("The default page", () => {
   it(`should render`, () => {
-    const subject = mount(<DefaultPage />)
+    const subject = mount(
+      <MockedProvider>
+        <DefaultPage />
+      </MockedProvider>
+    )
     expect(subject).toBeDefined()
   })
 })
