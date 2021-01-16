7734a01a5a12dad0fc5d71bbc1945b1b604d2412
Rob Brennan <rob@therobbrennan.com>
2021-01-16 01:33:58 -0800

    Added key things to review regarding custom resolvers

--- app/apollo/resolvers.ts
+++ app/apollo/resolvers.ts
@@ -1,30 +1,20 @@
-// Write resolvers to respond to our queries and mutations
-// https://www.notion.so/neo4j-graphql-v1-0-0-alpha-2-d47908030d4e4a0c86babbaef63887d0
+/**
+ * Write resolvers to respond to our queries and mutations
+ *
+ * Be sure to view the link below to see great examples of:
+ *  - Creating custom resolvers (e.g. mutation to handle creating a User node and send an automated email)
+ *  - Translate to override auto-generated resolvers by using the same name (e.g. createUsers with custom functionality)
+ *  - Use of the @cypher directive for any query or mutation (including in type definitions for properties such as relatedPosts)
+ *  - Use of the @auth directive for accepting JWTs in the request
+ *  - Use of the @exclude directive to exclude automatically generating queries or resolvers for types
+ *  - Use of the @autogenerate directive to automatically generate unique values for ID fields
+ *
+ *  https://www.notion.so/neo4j-graphql-v1-0-0-alpha-2-d47908030d4e4a0c86babbaef63887d0
+ */
 export const resolvers = {
   Query: {
     async hello(_parent, _args, _context) {
       return `Hello. The current timestamp is ${Date.now()}`
     },
   },
-
-  // EXAMPLE: Creating a custom resolver that sends an email while signing up a user in our database
-  //      See "Custom Resolvers" in https://www.notion.so/neo4j-graphql-v1-0-0-alpha-2-d47908030d4e4a0c86babbaef63887d0
-  // Mutation: {
-  //   async signUp(root, { email }, context) {
-  //     // Create user using manually entered Cypher
-  //     const cypher = "CREATE (u:User {email: $email}) RETURN n"
-  //     const params = { email }
-
-  //     const session = context.driver.session()
-  //     const userRes = await session.writeTransaction((tx) =>
-  //       tx.run(cypher, params)
-  //     )
-  //     session.close()
-
-  //     // Send email
-  //     context.emailService.sendWelcome(email)
-
-  //     return userRes.records[0].get(0)
-  //   },
-  // },
 }
