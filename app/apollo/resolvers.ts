c882b41fc2d430a9ac6b2f1a55a4ba7468719e41
Rob Brennan <rob@therobbrennan.com>
2021-01-16 01:24:26 -0800

    EXAMPLE: Creating a custom resolver that sends an email while signing up a user in our database

--- app/apollo/resolvers.ts
+++ app/apollo/resolvers.ts
@@ -6,4 +6,25 @@ export const resolvers = {
       return `Hello. The current timestamp is ${Date.now()}`
     },
   },
+
+  // EXAMPLE: Creating a custom resolver that sends an email while signing up a user in our database
+  //      See "Custom Resolvers" in https://www.notion.so/neo4j-graphql-v1-0-0-alpha-2-d47908030d4e4a0c86babbaef63887d0
+  // Mutation: {
+  //   async signUp(root, { email }, context) {
+  //     // Create user using manually entered Cypher
+  //     const cypher = "CREATE (u:User {email: $email}) RETURN n"
+  //     const params = { email }
+
+  //     const session = context.driver.session()
+  //     const userRes = await session.writeTransaction((tx) =>
+  //       tx.run(cypher, params)
+  //     )
+  //     session.close()
+
+  //     // Send email
+  //     context.emailService.sendWelcome(email)
+
+  //     return userRes.records[0].get(0)
+  //   },
+  // },
 }
